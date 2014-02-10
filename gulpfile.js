3e7addb50375197b24e2d25a3b50db165fcc06a1
Roberto Bonvallet <roberto@nimbic.com>
2014-02-10 14:28:47 -0300

    Install and set up jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
 
 gulp.task('default', function () {
 
+});
+
+gulp.task('lint', function () {
+    gulp.src('./*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
 });
