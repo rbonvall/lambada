550173e324f0225d3b95b736bdea60c9a3737f76
Roberto Bonvallet <roberto@nimbic.com>
2014-02-10 14:56:18 -0300

    Install and set up jasmine, add simple test

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
+var jasmine = require('gulp-jasmine');
 
 gulp.task('default', function () {
 
@@ -10,4 +11,9 @@ gulp.task('lint', function () {
     gulp.src('./*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('test', function () {
+    gulp.src('test.js')
+        .pipe(jasmine());
 });
---
+++ test.js
@@ -0,0 +1,7 @@
+var lambada = require('./lambada');
+
+describe('lambada', function () {
+    it('should return a function', function () {
+        expect(typeof lambada).toBe('function');
+    });
+});
