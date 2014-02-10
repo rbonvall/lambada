25129acc26f2e8b36e2a961165a405a00ce84160
Roberto Bonvallet <roberto@nimbic.com>
2014-02-10 16:12:11 -0300

    Lambda implementation from Functional Javascript
    This is copied almost verbatim from Functional Javascript's string
    lambda implementation by Oliver Steele:
    
      https://github.com/osteele/functional-javascript/blob/master/to-function.js
    
    with some minor changes:
    
    * expr is a parameter rather than this,
    * builtin split is used instead of custom ECMAsplit (ie no IE6 support)
    * shortcut conditional expression replaced by if statement.

--- lambada.js
+++ lambada.js
@@ -8,8 +8,45 @@
   }
 }(this, function () {
 
-    var lambada = function () {
 
+    var lambada = function (expr) {
+        var params = [],
+            sections = expr.split(/\s*->\s*/m);
+        if (sections.length > 1) {
+            while (sections.length) {
+                expr = sections.pop();
+                params = sections.pop().replace(/^\s*(.*)\s*$/, '$1').split(/\s*,\s*|\s+/m);
+                if (sections.length) {
+                    sections.push('(function('+params+'){return ('+expr+')})');
+                }
+            }
+        } else if (expr.match(/\b_\b/)) {
+            params = '_';
+        } else {
+            // test whether an operator appears on the left (or right), respectively
+            var leftSection = expr.match(/^\s*(?:[+*\/%&|\^\.=<>]|!=)/m),
+                rightSection = expr.match(/[+\-*\/%&|\^\.=<>!]\s*$/m);
+            if (leftSection || rightSection) {
+                if (leftSection) {
+                    params.push('$1');
+                    expr = '$1' + expr;
+                }
+                if (rightSection) {
+                    params.push('$2');
+                    expr = expr + '$2';
+                }
+            } else {
+                // `replace` removes symbols that are capitalized, follow '.',
+                // precede ':', are 'true', 'false', 'null', 'undefined', 'this'
+                // or 'arguments'; and also the insides of strings (by a crude test).
+                // `match` extracts the remaining symbols.
+                var vars = expr.replace(/(?:\b[A-Z]|\.[a-zA-Z_$])[a-zA-Z_$\d]*|[a-zA-Z_$][a-zA-Z_$\d]*\s*:|true|false|null|undefined|this|arguments|'(?:[^'\\]|\\.)*'|"(?:[^"\\]|\\.)*"/g, '').match(/([a-z_$][a-z_$\d]*)/gi) || []; // '
+                for (var i = 0, v; v = vars[i]; i++) {
+                    params.indexOf(v) >= 0 || params.push(v);
+                }
+            }
+        }
+        return new Function(params, 'return (' + expr + ')');
     };
 
     return lambada;
